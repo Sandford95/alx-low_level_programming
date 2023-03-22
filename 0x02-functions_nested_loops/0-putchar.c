e4dac5cbe5631bb93ed971cb81f71df007e699b3
Sandford95 <Sngalamila9@gmai.com>
2023-03-22 11:33:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * main - prints the string "Sandford" from a character array.
+ * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	-putchar('_');
-	-putchar('p');
-	-putchar('u');
-	-putchar('t');
-	-putchar('c');
-	-putchar('h');
-	-putchar('a');
-	-putchar('r');
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
