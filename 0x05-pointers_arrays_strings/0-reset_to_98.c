fc019fed42436898f89cbeb040011041b2b8f97f
Sandford95 <Sngalamila9@gmai.com>
2023-03-28 13:15:40 -0700

    correction to 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 /**
- * main- print the value of n to 98
- * Return: 0 for success
+ * reset_to_98 - updates value of pointer to 98.
+ * @n: input to update to 98.
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;	
+	*n = 98;
 }
